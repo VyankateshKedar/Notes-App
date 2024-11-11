38dd9bf3b07c236f5e5b96104210a43970eeb6f4
Vyankatesh <125432163+VyankateshKedar@users.noreply.github.com>
2024-11-12 00:35:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Notes-App/', // your repository name here
 })
