0fca54808a168893934285c83b5269a510276886
Vyankatesh <125432163+VyankateshKedar@users.noreply.github.com>
2024-11-13 18:09:10 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,8 @@
 // src/components/Sidebar.js
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Sidebar.css';
-import { v4 as uuidv4 } from 'uuid';
-import GroupPopup from './GroupPopup';
 
 const Sidebar = ({ groups, onSelectGroup, onCreateGroup }) => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  const handleAddGroup = () => {
-    setIsPopupOpen(true);
-  };
-
-  const handleCreateGroup = (name, color) => {
-    const newGroup = {
-      id: uuidv4(),
-      name,
-      initials: generateInitials(name),
-      color,
-    };
-
-    onCreateGroup(newGroup); // Notify App about the new group
-    setIsPopupOpen(false);
-  };
-
-  const generateInitials = (name) => {
-    const words = name.split(' ');
-    if (words.length === 1) {
-      return words[0][0].toUpperCase();
-    } else {
-      return (words[0][0] + words[1][0]).toUpperCase();
-    }
-  };
-
   return (
     <div className="sidebar">
       <h2>Pocket Notes</h2>
@@ -52,16 +23,9 @@ const Sidebar = ({ groups, onSelectGroup, onCreateGroup }) => {
           </div>
         ))}
       </div>
-      <button className="add-group-btn" onClick={handleAddGroup}>
+      <button className="add-group-btn" onClick={onCreateGroup}>
         +
       </button>
-
-      {isPopupOpen && (
-        <GroupPopup
-          onClose={() => setIsPopupOpen(false)}
-          onCreate={handleCreateGroup}
-        />
-      )}
     </div>
   );
 };
